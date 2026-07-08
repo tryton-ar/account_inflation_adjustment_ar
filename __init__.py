0190685087fe67cd0676f529b4551fe38b7ce921
Luciano Rossi <lukio@gcoop.coop>
2026-07-08 04:54:30 -0300

    Use to register in the Pool

--- __init__.py
+++ __init__.py
@@ -1,25 +1,3 @@
 # This file is part of the account_inflation_adjustment_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import account
-from . import move
-
-__all__ = ['register']
-
-
-def register():
-    Pool.register(
-        account.Period,
-        account.Account,
-        account.InflationAdjustment,
-        account.InflationAdjustmentPeriod,
-        account.SetAccountInflationAdjustableStart,
-        move.Move,
-        move.Line,
-        module='account_inflation_adjustment_ar', type_='model')
-    Pool.register(
-        account.SetAccountInflationAdjustable,
-        account.OpenInflationAdjustmentMove,
-        module='account_inflation_adjustment_ar', type_='wizard')
